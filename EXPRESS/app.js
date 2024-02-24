f188fd9d4bfbcb237399d043d8583e90f04977de
Serkan <s8ersis@gmail.com>
2024-02-24 07:07:46 +0300

    express params

--- EXPRESS/app.js
+++ EXPRESS/app.js
@@ -1,7 +1,36 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+mockUsers = [
+    {id: 1, username: "Ali", firstName: "Abdullah"}, 
+    {id: 2, username: "Veli", firstName: "Hakan"}, 
+    {id: 3, username: "Danny", firstName: "Deli"} 
+]
+
+
+app.get('/', (req, res) => res.send('Hello, Welcome to our World!'))
+app.get("/users", (req, res) => {
+    res.send(mockUsers)
+})
+
+app.get("/users/:id", (req, res) => {
+    console.log(req.params);
+    const parsedId = parseInt(req.params.id)
+    const findUser = mockUsers.find(user => user.id === parsedId)
+    isNaN(parsedId) ? res.status(400).send({message: "Bad Request, INVALID ID"}): findUser ? res.send(findUser) : res.sendStatus(404)
+
+    
+
+})
+
+app.get("/products", (req, res) => {
+    res.send([
+        {id:1, productName:"LG", price: 150},
+        {id:2, productName:"TOSHIBA", price: 170},
+        {id:3, productName:"SAMSUNG", price: 180}
+    ])
+})
+
+app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
