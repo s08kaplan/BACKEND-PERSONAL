7939921460ae6fcea885ae8676da56793ddc660d
Serkan <s8ersis@gmail.com>
2024-03-12 07:02:27 +0300

    server function url fixed

--- PROJECTS/EXPRESS-SEQUELIZE/only-books/FRONTEND/books/src/helper/serverFunction.js
+++ PROJECTS/EXPRESS-SEQUELIZE/only-books/FRONTEND/books/src/helper/serverFunction.js
@@ -1,7 +1,7 @@
 export const serverFunction = async (url = "books", info) => {
   const BASE_URL = import.meta.env.VITE_BASE_URL;
   const sendData = JSON.stringify(info)
-  const res = await fetch(BASE_URL/url,{
+  const res = await fetch(`${BASE_URL}/${url}`,{
     method:"POST",
     body:sendData
 });
