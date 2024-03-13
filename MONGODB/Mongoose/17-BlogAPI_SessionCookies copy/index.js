44283c16edb30ca1d0d59e8d637e66c1308c9ea3
Serkan <s8ersis@gmail.com>
2024-03-13 23:29:10 +0300

    session cookies added

---
+++ MONGODB/Mongoose/17-BlogAPI_SessionCookies copy/index.js
@@ -0,0 +1,55 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG API
+------------------------------------------------------- */
+/*
+    BLOG API with Mongoose
+*/
+/*
+    $ npm i express dotenv express-async-errors
+    $ npm i mongoose
+*/
+const express = require("express")
+const app = express()
+
+
+app.use(express.json()) // yukarıda  kalsın
+
+require('dotenv').config()
+const PORT = process.env.PORT
+const HOST = process.env.HOST
+
+/* DB connection  */
+require('./src/configs/dbConnection') // dotenv çalıştıktan sonra 
+
+/* ------------------------------------------------------- */
+// SessionCookies:
+// http://expressjs.com/en/resources/middleware/cookie-session.html
+// https://www.npmjs.com/package/cookie-session
+//* $ npm i cookie-session
+
+const session = require("cookie-session")
+
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    // maxAge:1000 * 60 
+}))
+/* ------------------------------------------------------- */
+
+app.all('/', (req, res) => {
+    // res.send('WELCOME BLOG API PROJECT')
+    res.send({
+        error: false,
+        message:'WELCOME BLOG API PROJECT',
+        loggedInUser: req.session
+        })
+})
+
+app.use('/user', require("./src/routes/user.router"))
+app.use('/blog', require("./src/routes/blog.router"))
+
+app.use(require('./src/middlewares/errorHandler')) // aşağıda kalsın
+
+app.listen(PORT, () => console.log(` Server Running on http://${HOST}:${PORT}`))
+
+// require('./src/sync')()
---
+++ MONGODB/Mongoose/17-BlogAPI_SessionCookies copy/src/controllers/user.controller.js
@@ -0,0 +1,93 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - BLOG API
+------------------------------------------------------- */
+
+require("express-async-errors");
+
+const User = require("../models/user.model");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+module.exports = {
+  list: async (req, res) => {
+    const data = await User.find();
+    res.status(200).send({
+      error: false,
+      data: data,
+    });
+  },
+  create: async (req, res) => {
+    const data = await User.create(req.body);
+    res.status(201).send({
+      error: false,
+      body: req.body,
+      data: data,
+    });
+  },
+  read: async (req, res) => {
+    const data = await User.findOne({ _id: req.params.userId });
+    res.status(202).send({
+      error: false,
+      data: data,
+    });
+  },
+  update: async (req, res) => {
+    const data = await User.updateOne({ _id: req.params.userId }, req.body);
+    const newdata = await User.findOne({ _id: req.params.userId });
+    res.status(202).send({
+      error: false,
+      body: req.body,
+      data: data, // info about update
+      // güncel veriyi istiyorsan tekrar çağır
+      newdata: newdata,
+    });
+  },
+  delete: async (req, res) => {
+    const data = await User.deleteOne({ _id: req.params.userId });
+    // console.log(data);
+    res.sendStatus(data.deletedCount >= 1 ? 204 : 404);
+  },
+
+  login: async (req, res) => {
+    const { email, password } = req.body;
+    if (email && password) {
+      // const user = await User.findOne({ email: email})
+      const user = await User.findOne({ email });
+      if (user && user.password == passwordEncrypt(password)) {
+
+        // req.session = {
+        //     email: user.email,
+        //     password: user.password
+        // }
+
+        req.session.email = user.email
+        req.session.password = user.password
+
+        if(req.body?.remindMe) {
+            req.session.remindMe = req.body.remindMe
+            req.sessionOptions.maxAge = 1000 * 60 * 60 * 24 * 3
+        }
+
+        res.status(200).send({
+          error: false,
+          message: "Login OK",
+          user,
+        });
+      } else {
+        res.errorStatusCode = 401;
+        throw new Error("Login parameters are not true.");
+      }
+    } else {
+      res.errorStatusCode = 401;
+      throw new Error("Email and password are required.");
+    }
+  },
+  logout: async (req, res) => {
+
+    req.session = null
+    res.status(200).send({
+        error: false,
+        message:"Logged out Successfully"
+    })
+  },
+};
