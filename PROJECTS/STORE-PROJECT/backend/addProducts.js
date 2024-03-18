29b57e9e5193f13560880167b94de3a8e919f2f8
Serkan <s8ersis@gmail.com>
2024-03-18 20:52:59 +0300

    backend products to display handling

--- PROJECTS/STORE-PROJECT/backend/addProducts.js
+++ PROJECTS/STORE-PROJECT/backend/addProducts.js
@@ -4,4 +4,4 @@ const router = require("./src/routes/products.router")
 const Products = require("./src/models/products.model")
 const pro = require("./src/controllers/products.controller")
 
-router.post("/products",)
+router.post("/products")
