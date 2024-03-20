3da2ad8d6db8799bfe2d0d7c38a1b18dcb14f573
Serkan <s8ersis@gmail.com>
2024-03-20 23:48:52 +0300

    simple token

---
+++ MONGODB/20-PersonnelAPI_Token_Perm/src/controllers/auth.controller.js
@@ -0,0 +1,83 @@
+"use strict"
+/* -------------------------------------------------------
+    EXPRESS - Personnel API
+------------------------------------------------------- */
+
+const Personnel = require('../models/personnel.model')
+const Token = require('../models/token.model')
+const passwordEncrypt = require('../helpers/passwordEncrypt')
+
+module.exports = {
+
+    // LOGIN & LOGOUT
+
+    login: async (req, res) => {
+
+        const { username, password } = req.body
+
+        if (username && password) {
+
+            //? findOne, passwordu modeldeki set metodundaki encrypt i kullanarak db'de filtreleme yapar
+            const user = await Personnel.findOne({ username, password })
+            if (user) {
+
+                /* SESSION *
+                
+                // Set Session:
+                req.session = {
+                    id: user._id,
+                    password: user.password
+                }
+                // Set Cookie:
+                if (req.body?.rememberMe) {
+                    req.sessionOptions.maxAge = 1000 * 60 * 60 * 24 * 3 // 3 Days
+                }
+
+                /* SESSION */
+
+                /* TOKEN */
+
+                // Token var mı?
+                let tokenData = await Token.findOne({ userId: user._id })
+
+                // Eğer token yoksa oluştur:
+                if (!tokenData) {
+                    const tokenKey = passwordEncrypt(user._id + Date.now())
+                    // console.log(typeof tokenKey, tokenKey)
+                    tokenData = await Token.create({ userId: user._id, token: tokenKey })
+                }
+                
+                /* TOKEN */
+
+                res.status(200).send({
+                    error: false,
+                    token: tokenData.token,
+                    user
+                })
+
+            } else {
+                res.errorStatusCode = 401
+                throw new Error('Wrong Username or Password.')
+            }
+        } else {
+            res.errorStatusCode = 401
+            throw new Error('Please enter username and password.')
+        }
+    },
+
+    logout: async (req, res) => {
+        /* SESSION */
+        // Set session to null:
+        req.session = null
+        /* SESSION */
+
+        /* TOKEN */
+
+        /* TOKEN */
+
+        res.status(200).send({
+            error: false,
+            message: 'Logout: Sessions Deleted.'
+        })
+    },
+}
